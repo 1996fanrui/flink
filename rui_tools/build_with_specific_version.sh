4c83eeb9d12b3401f1ee38c60d3fde7b1680dd12
Rui Fan <1996fanrui@gmail.com>
2026-02-19 23:15:57 +0100

    Avoid zsh failure

--- rui_tools/build_with_specific_version.sh
+++ rui_tools/build_with_specific_version.sh
@@ -19,13 +19,13 @@
 
 ################################################################################
 
-set -e  # 遇到错误立即退出
-# set -x  # 会在执行每个命令之前，先打印出这个命令以及其所有参数。方便排查具体执行到哪里了
-
 # Source the user's profile to load environment variables
 if [ -f ~/.zshrc ]; then
     . ~/.zshrc
 fi
+
+set -e  # 遇到错误立即退出
+# set -x  # 会在执行每个命令之前，先打印出这个命令以及其所有参数。方便排查具体执行到哪里了
 
 export FLINK_VERSION="os-troubleshooting-$(git rev-parse --short HEAD)"
 
